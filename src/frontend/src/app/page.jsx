5562eec89ffde5bac109b6738668eb0fbe489274
Javier Ezpeleta <javierezpeleta@gmail.com>
2024-01-15 14:10:14 +0100

    adding API Key field + a bit of redesign

--- src/frontend/src/app/page.jsx
+++ src/frontend/src/app/page.jsx
@@ -1,44 +1,109 @@
 "use client"
 
-import Image from 'next/image'
-import { useWebsiteConfig } from '../lib/state'
-import { generateWebsite } from '../lib/api'
+import { useWebsiteConfig } from "../lib/state"
+import { generateWebsite } from "../lib/api"
+import HomePageDecorations from "@/components/HomePageDecorations"
 
 export default function Home() {
   const {
     prompt,
     setPrompt,
+    api_key,
+    setAPIKey,
     generate_js,
     setGenerateJS,
     generate_css,
     setGenerateCSS,
-    generate_inlinecss, 
-    setGenerateInlineCSS
+    generate_inlinecss,
+    setGenerateInlineCSS,
   } = useWebsiteConfig()
 
   return (
-    <main className="flex min-h-screen flex-col items-center space-y-12 p-24">
-      <h1 className="text-4xl font-bold text-center">Generate your own website!</h1>
-      <form className='space-x-4 space-y-8' action={generateWebsite}>
-        <input name='prompt' type="text" placeholder="Website idea" className="border-2 border-gray-300 rounded-md px-8 py-4" onChange={(e) => setPrompt(e.target.value)} />
-        <button type="submit" className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-4 px-6 rounded'>Generate</button>
+    <main className="flex min-h-screen flex-col items-center space-y-4 p-4 py-24 sm:p-24">
+      <HomePageDecorations />
+      <h1 className="text-2xl sm:text-4xl font-bold text-center">Header</h1>
+      <h1 className="text-lg sm:text-2xl font-bold text-center">Subheader</h1>
+      <form
+        className="space-y-4 max-w-lg w-full flex flex-col bg-gradient-to-br from-indigo-100 to-purple-200 p-8 rounded-lg"
+        action={generateWebsite}
+      >
+        <input
+          name="prompt"
+          type="text"
+          placeholder="Website idea"
+          className="border-2 border-gray-300 rounded-md px-4 py-2"
+          onChange={(e) => setPrompt(e.target.value)}
+        />
 
-        <div className="grid grid-rows-2">
-          <div className='flex items-center mb-4 space-y-4 space-x-4'>
-              <input id="generate_js" name="generate_js" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={(e) => setGenerateJS(e.target.checked)} checked={generate_js} />
-              <label htmlFor="generate_js" className="ms-2 text-sm font-medium">Generate JS</label>
-              <input id="generate_css" name='generate_css' type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={(e) => setGenerateCSS(e.target.checked)} checked={generate_css} />
-              <label htmlFor="generate_css" className="ms-2 text-sm font-medium">Generate CSS</label>
-          </div>
-          <div className='flex items-center'>
-              <input id="generate_inlinecss" name="generate_inlinecss" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" onChange={(e) => setGenerateInlineCSS(e.target.checked)} checked={generate_inlinecss} />
-              <label htmlFor="generate_inlinecss" className="ms-2 text-sm font-medium" >Generate Inline CSS</label>
+        <div className="flex items-center space-y-4 space-x-4 pb-4">
+          <div className="flex flex-wrap items-center justify-center gap-x-8 gap-y-4 mx-auto max-w-sm">
+            <div className="flex items-center">
+              <input
+                id="generate_js"
+                name="generate_js"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded 
+                    focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 
+                    focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                onChange={(e) => setGenerateJS(e.target.checked)}
+                checked={generate_js}
+              />
+              <label htmlFor="generate_js" className="ms-2 text-sm font-medium">
+                Generate JS
+              </label>
+            </div>
+            <div className="flex items-center">
+              <input
+                id="generate_css"
+                name="generate_css"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                onChange={(e) => setGenerateCSS(e.target.checked)}
+                checked={generate_css}
+              />
+              <label htmlFor="generate_css" className="ms-2 text-sm font-medium">
+                Generate CSS
+              </label>
+            </div>
+            <div className="flex items-center">
+              <input
+                id="generate_inlinecss"
+                name="generate_inlinecss"
+                type="checkbox"
+                value=""
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                onChange={(e) => setGenerateInlineCSS(e.target.checked)}
+                checked={generate_inlinecss}
+              />
+              <label htmlFor="generate_inlinecss" className="ms-2 text-sm font-medium">
+                Generate Inline CSS
+              </label>
+            </div>
           </div>
         </div>
+
+        <input
+          name="prompt"
+          type="text"
+          placeholder="Enter your API Key here"
+          className="border-2 border-gray-300 rounded-md px-4 py-2"
+          onChange={(e) => setAPIKey(e.target.value)}
+        />
+
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+        >
+          Generate
+        </button>
       </form>
 
       <div className="flex flex-col items-center mt-20">
-        <pre>{JSON.stringify({ prompt, generate_js, generate_css, generate_inlinecss }, null, 2)}</pre>
+        <pre>
+          {JSON.stringify({ prompt, generate_js, generate_css, generate_inlinecss, api_key }, null, 2)}
+        </pre>
       </div>
     </main>
   )
--- src/frontend/src/lib/state.ts
+++ src/frontend/src/lib/state.ts
@@ -1,34 +1,37 @@
-import { create } from "zustand";
+import { create } from "zustand"
 
 type WebsiteConfig = {
-    prompt: string;
-    generate_js: boolean;
-    generate_css: boolean;
-    generate_inlinecss: boolean;
+  prompt: string
+  api_key: string
+  generate_js: boolean
+  generate_css: boolean
+  generate_inlinecss: boolean
 }
 
 export const useWebsiteConfig = create<WebsiteConfig>((set) => ({
-    prompt: "",
-    generate_js: true,
-    generate_css: true,
-    generate_inlinecss: false,
+  prompt: "",
+  api_key: "",
+  generate_js: true,
+  generate_css: true,
+  generate_inlinecss: false,
 
-    setPrompt: (prompt: string) => set({ prompt }),
-    setGenerateJS: (generate_js: boolean) => set({ generate_js }),
-    setGenerateCSS: (generate_css: boolean) => {
-        if (generate_css) {
-            set({ generate_css });
-            set({ generate_inlinecss: false }); 
-        } else {
-            set({ generate_css });
-        }
-    },
-    setGenerateInlineCSS: (generate_inlinecss: boolean) => {
-        if (generate_inlinecss) {
-            set({ generate_inlinecss });
-            set({ generate_css: false }); 
-        } else {
-            set({ generate_inlinecss });
-        }
-    },
-}));
+  setPrompt: (prompt: string) => set({ prompt }),
+  setAPIKey: (api_key: string) => set({ api_key }),
+  setGenerateJS: (generate_js: boolean) => set({ generate_js }),
+  setGenerateCSS: (generate_css: boolean) => {
+    if (generate_css) {
+      set({ generate_css })
+      set({ generate_inlinecss: false })
+    } else {
+      set({ generate_css })
+    }
+  },
+  setGenerateInlineCSS: (generate_inlinecss: boolean) => {
+    if (generate_inlinecss) {
+      set({ generate_inlinecss })
+      set({ generate_css: false })
+    } else {
+      set({ generate_inlinecss })
+    }
+  },
+}))
